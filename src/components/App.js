0dec136d02f07ac5c380cefd99f4764dc80c2b51
Shaundai Person <shaundaiperson@gmail.com>
2020-01-01 23:02:45 -0500

    complete functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import Game from './Game';
 
 class App extends React.Component{
+  state = {
+    gameId: 1,
+  }
+
+  resetGame = () => {
+    this.setState((prevState) => {
+      return {
+        gameId: prevState.gameId + 1
+      }
+    })
+  }
+// adding a key to this App component is an easy trick for remounting and starting new game
   render(){
     return (
-      <Game randomNumberCount={6} />
+      <Game key={this.state.gameId}
+      onPlayAgain={this.resetGame}
+      randomNumberCount={6}
+      initialSeconds={10} />
     );
   }
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import PropTypes from 'prop-types';
 import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
 
 class Game extends React.Component {
     static propTypes = {
         randomNumberCount: PropTypes.number.isRequired,
+        initialSeconds: PropTypes.number.isRequired,
+        onPlayAgain: PropTypes.func.isRequired,
     };
 
     state = {
         selectedIds: [],
+        remainingSeconds: this.props.initialSeconds,
     }
-
+    
+    gameStatus = 'PLAYING';
     randomNumbers = Array.from({ length: this.props.randomNumberCount }).map(()=> 1 + Math.floor(40 * Math.random()))
     target = this.randomNumbers.slice(0, this.props.randomNumberCount - 2).reduce ((acc, curr) => acc + curr, 0);
-    
+    shuffledRandomNumbers = shuffle(this.randomNumbers)
+
+    componentDidMount(){
+        this.intervalId = setInterval(()=> {
+            this.setState((prevState) => {
+                return { remainingSeconds: prevState.remainingSeconds - 1 };
+            },() => {
+                if(this.state.remainingSeconds === 0){
+                clearInterval(this.intervalId)
+            }
+            });
+        }, 1000)
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        if(nextState.selectedIds !== this.state.selectedIds || nextState.remainingSeconds === 0){
+            this.gameStatus = this.calcGameStatus(nextState);
+            if(this.gameStatus !== 'PLAYING'){
+                clearInterval(this.intervalId)
+            }
+        }
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId)
+    }
+
     isNumberDisabled = (numberIndex) => {
         return this.state.selectedIds.indexOf(numberIndex) >= 0;
     }
@@ -25,11 +56,14 @@ class Game extends React.Component {
         })
         )
     }
-
-    gameStatus = () => {
-        const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
-            return acc + this.randomNumbers[curr]
+    
+    calcGameStatus = (nextState) => {
+        const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+            return acc + this.shuffledRandomNumbers[curr]
         }, 0);
+        if(nextState.remainingSeconds === 0){
+            return 'LOST'
+        }
         if(sumSelected < this.target){
             return 'PLAYING'
         }
@@ -42,32 +76,44 @@ class Game extends React.Component {
     }
 
     render(){
-        const gameStatus = this.gameStatus();
+        const gameStatus = this.gameStatus;
         return(
             <View style={styles.container}>
-                <Text style={styles.target}>{this.target}</Text>
+                <Text style={styles.instructions}>Choose the numbers that add up to the target number.  Don't go over or you'll lose!</Text>
+                <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>{this.target}</Text>
                 <View style={styles.randomContainer}>
-                {this.randomNumbers.map((randomNumber, index) =>
+                {this.shuffledRandomNumbers.map((randomNumber, index) =>
                 <RandomNumber key={index}
                 id={index}
                 number={randomNumber}
-                isDisabled={this.isNumberDisabled(index)}
+                isDisabled={this.isNumberDisabled(index) || gameStatus !== ('PLAYING')}
                 onPress={this.selectNumber}
                 />
-                    //dont forget to shuffle the randomNumbers
                 )}
                 </View>
-                <Text>{gameStatus}</Text>
+                {this.gameStatus !== 'PLAYING' && (
+                <Button title="Play Again" onPress={this.props.onPlayAgain}/>
+                )}
+                <Text>{this.gameStatus}</Text>
+                <Text>{this.state.remainingSeconds}</Text>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    instructions: {
+        fontSize: 20,
+        paddingTop: 30,
+        textAlign: 'center',
+        flex: .25,
+    },
     container: {
         backgroundColor: '#ddd',
         flex: 1,
         paddingTop: 30,
+        paddingRight: 30,
+        paddingLeft: 30,
     },
     target: {
         fontSize: 40,
@@ -82,6 +128,15 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flex: 1,
         margin: 40,
+    },
+    STATUS_PLAYING: {
+        backgroundColor: '#bbb'
+    },
+    STATUS_WON: {
+        backgroundColor: 'green'
+    },
+    STATUS_LOST: {
+        backgroundColor: 'red'
     },
 
     numbers: {
